3c47cf9cd82273be43c7aaf4886261fdf11b1222
arvinsadeli <arvinsadeli1@gmail.com>
2023-08-22 00:55:47 +0700

    First Push

---
+++ ElementalEncounters/Intermediate/Build/Win64/UnrealEditor/Inc/ElementalEncounters/UHT/ElementalEncountersCharacter.generated.h
@@ -0,0 +1,96 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "ElementalEncountersCharacter.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ELEMENTALENCOUNTERS_ElementalEncountersCharacter_generated_h
+#error "ElementalEncountersCharacter.generated.h already included, missing '#pragma once' in ElementalEncountersCharacter.h"
+#endif
+#define ELEMENTALENCOUNTERS_ElementalEncountersCharacter_generated_h
+
+#define FID_Project_Elemental_Encounters_ElementalEncounters_Source_ElementalEncounters_ElementalEncountersCharacter_h_14_SPARSE_DATA
+#define FID_Project_Elemental_Encounters_ElementalEncounters_Source_ElementalEncounters_ElementalEncountersCharacter_h_14_RPC_WRAPPERS
+#define FID_Project_Elemental_Encounters_ElementalEncounters_Source_ElementalEncounters_ElementalEncountersCharacter_h_14_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_Project_Elemental_Encounters_ElementalEncounters_Source_ElementalEncounters_ElementalEncountersCharacter_h_14_ACCESSORS
+#define FID_Project_Elemental_Encounters_ElementalEncounters_Source_ElementalEncounters_ElementalEncountersCharacter_h_14_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAElementalEncountersCharacter(); \
+	friend struct Z_Construct_UClass_AElementalEncountersCharacter_Statics; \
+public: \
+	DECLARE_CLASS(AElementalEncountersCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/ElementalEncounters"), NO_API) \
+	DECLARE_SERIALIZER(AElementalEncountersCharacter)
+
+
+#define FID_Project_Elemental_Encounters_ElementalEncounters_Source_ElementalEncounters_ElementalEncountersCharacter_h_14_INCLASS \
+private: \
+	static void StaticRegisterNativesAElementalEncountersCharacter(); \
+	friend struct Z_Construct_UClass_AElementalEncountersCharacter_Statics; \
+public: \
+	DECLARE_CLASS(AElementalEncountersCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/ElementalEncounters"), NO_API) \
+	DECLARE_SERIALIZER(AElementalEncountersCharacter)
+
+
+#define FID_Project_Elemental_Encounters_ElementalEncounters_Source_ElementalEncounters_ElementalEncountersCharacter_h_14_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AElementalEncountersCharacter(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AElementalEncountersCharacter) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AElementalEncountersCharacter); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AElementalEncountersCharacter); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AElementalEncountersCharacter(AElementalEncountersCharacter&&); \
+	NO_API AElementalEncountersCharacter(const AElementalEncountersCharacter&); \
+public: \
+	NO_API virtual ~AElementalEncountersCharacter();
+
+
+#define FID_Project_Elemental_Encounters_ElementalEncounters_Source_ElementalEncounters_ElementalEncountersCharacter_h_14_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AElementalEncountersCharacter(AElementalEncountersCharacter&&); \
+	NO_API AElementalEncountersCharacter(const AElementalEncountersCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AElementalEncountersCharacter); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AElementalEncountersCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AElementalEncountersCharacter) \
+	NO_API virtual ~AElementalEncountersCharacter();
+
+
+#define FID_Project_Elemental_Encounters_ElementalEncounters_Source_ElementalEncounters_ElementalEncountersCharacter_h_11_PROLOG
+#define FID_Project_Elemental_Encounters_ElementalEncounters_Source_ElementalEncounters_ElementalEncountersCharacter_h_14_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Project_Elemental_Encounters_ElementalEncounters_Source_ElementalEncounters_ElementalEncountersCharacter_h_14_SPARSE_DATA \
+	FID_Project_Elemental_Encounters_ElementalEncounters_Source_ElementalEncounters_ElementalEncountersCharacter_h_14_RPC_WRAPPERS \
+	FID_Project_Elemental_Encounters_ElementalEncounters_Source_ElementalEncounters_ElementalEncountersCharacter_h_14_ACCESSORS \
+	FID_Project_Elemental_Encounters_ElementalEncounters_Source_ElementalEncounters_ElementalEncountersCharacter_h_14_INCLASS \
+	FID_Project_Elemental_Encounters_ElementalEncounters_Source_ElementalEncounters_ElementalEncountersCharacter_h_14_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_Project_Elemental_Encounters_ElementalEncounters_Source_ElementalEncounters_ElementalEncountersCharacter_h_14_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Project_Elemental_Encounters_ElementalEncounters_Source_ElementalEncounters_ElementalEncountersCharacter_h_14_SPARSE_DATA \
+	FID_Project_Elemental_Encounters_ElementalEncounters_Source_ElementalEncounters_ElementalEncountersCharacter_h_14_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Project_Elemental_Encounters_ElementalEncounters_Source_ElementalEncounters_ElementalEncountersCharacter_h_14_ACCESSORS \
+	FID_Project_Elemental_Encounters_ElementalEncounters_Source_ElementalEncounters_ElementalEncountersCharacter_h_14_INCLASS_NO_PURE_DECLS \
+	FID_Project_Elemental_Encounters_ElementalEncounters_Source_ElementalEncounters_ElementalEncountersCharacter_h_14_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ELEMENTALENCOUNTERS_API UClass* StaticClass<class AElementalEncountersCharacter>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Project_Elemental_Encounters_ElementalEncounters_Source_ElementalEncounters_ElementalEncountersCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ ElementalEncounters/Source/ElementalEncounters/ElementalEncounters.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ElementalEncounters.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ElementalEncounters, "ElementalEncounters" );
+ 
