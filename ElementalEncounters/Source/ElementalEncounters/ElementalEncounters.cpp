56890de2bd793644664a74ea34cfa281f9b96122
arvinsadeli <arvinsadeli1@gmail.com>
2023-08-22 00:46:46 +0700

    .

--- ElementalEncounters/Source/ElementalEncounters/ElementalEncounters.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ElementalEncounters.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ElementalEncounters, "ElementalEncounters" );
- 
